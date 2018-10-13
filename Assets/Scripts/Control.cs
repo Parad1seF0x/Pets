82861e93e5044c6c894835a0e057255dd0a9a4d4
Parad1seF0x <43928323+Parad1seF0x@users.noreply.github.com>
2018-10-13 14:57:31 +0300

    Creating Unity project

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class Control : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    bool pressed;
+    public GameObject thisgo;
+    int aroow;
+
+    private void Start()
+    {
+
+       
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+        if (thisgo.tag == "RArrow")
+        {
+            aroow = 0;
+        }
+        else
+        {
+            aroow = 1;
+        }
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!pressed)
+            return;
+
+        if (aroow == 0)
+        {
+            Debug.Log("Pressed Right");
+        }
+        else
+        {
+            Debug.Log("Pressed Left");
+        }
+
+        
+        
+
+    }
+}
---
+++ Assets/Scripts/loading.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loading : MonoBehaviour {
+
+    public GameObject thisgo;
+    public GameObject foledr;
+    public GameObject Room1;
+    public GameObject cam;
+    public GameObject gc;
+
+    // Use this for initialization
+    void Start () {
+        thisgo = this.gameObject;
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        Room1 = GameObject.FindGameObjectWithTag("Room1");
+        gc = GameObject.FindGameObjectWithTag("GameController");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (thisgo.transform.position.x < 0) {
+            thisgo.transform.position += new Vector3(Time.deltaTime, 0, 0);
+            thisgo.transform.localScale += new Vector3(Time.deltaTime * 2, 0, 0);
+        }
+        else
+        {
+            cam.transform.position = new Vector3(Room1.transform.position.x, Room1.transform.position.y - 0.5f, -10f);
+
+            gc.SetActive(true);
+            foledr.SetActive(false);
+
+        }
+    }
+}
